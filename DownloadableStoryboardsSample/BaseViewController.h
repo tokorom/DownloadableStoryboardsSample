af302dcf5cf2fe6271960670eee7939cff37fe1e
tokorom <yuta-tokoro@cookpad.com>
2013-11-13 17:22:56 +0900

    Update

--- DownloadableStoryboardsSample/BaseViewController.h
+++
@@ -1,8 +0,0 @@
-//
-//  BaseViewController.h
-//
-//  Created by ToKoRo on 2013-11-12.
-//
-
-@interface BaseViewController : UIViewController
-@end
--- DownloadableStoryboardsSample/ViewController.m
+++ DownloadableStoryboardsSample/ViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "SSZipArchive.h"
 
-@interface ViewController ()
-
+@interface ViewController () <SSZipArchiveDelegate>
 @end
 
 @implementation ViewController
@@ -17,13 +17,65 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)startDownload
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+
+    NSURLSession* session = [NSURLSession sharedSession];
+    NSURL* url = [NSURL URLWithString:@"https://dl.dropboxusercontent.com/u/10351676/storyboards.bundle.zip"];
+
+    NSURLSessionTask* task;
+    task = [session dataTaskWithURL:url
+                  completionHandler:^(NSData* data, NSURLResponse* response, NSError* error)
+    {
+        if (error) {
+            NSLog(@"Error: %@", error);
+        } else {
+            NSString* zipFileName = [self writeZipToFileWithData:data];
+            [self unzipBundleWithZipFileName:zipFileName];
+        }
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+    }];
+    [task resume];
 }
 
+- (NSString*)writeZipToFileWithData:(NSData*)data
+{
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString* documentsDirectory = [paths objectAtIndex:0];
+    NSString* path = [documentsDirectory stringByAppendingPathComponent:@"storyboards.zip"];
+    [data writeToFile:path atomically:YES]; 
+    return path;
+}
+
+- (NSString*)unzipBundleWithZipFileName:(NSString*)zipFileName
+{
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString* documentsDirectory = [paths objectAtIndex:0];
+    NSString* path = [documentsDirectory stringByAppendingPathComponent:@""];
+    [SSZipArchive unzipFileAtPath:zipFileName toDestination:path delegate:self];
+    return path;
+}
+
+- (void)zipArchiveDidUnzipArchiveAtPath:(NSString*)path zipInfo:(unz_global_info)zipInfo unzippedPath:(NSString*)unzippedPath
+{
+    NSLog(@"unzipped: %@", unzippedPath);
+    NSString* bundlePath = [unzippedPath stringByAppendingPathComponent:@"storyboards.bundle"];
+    NSLog(@"bundlePath: %@", bundlePath);
+
+    NSURL* url = [NSURL fileURLWithPath:bundlePath];
+    NSBundle* bundle = [NSBundle bundleWithURL:url];
+
+    NSLog(@"bundle: %@", bundle);
+
+    UIStoryboard* storyboard = [UIStoryboard storyboardWithName:@"Downloadable" bundle:bundle];
+    NSLog(@"storyboard: %@", storyboard);
+    UIViewController* viewController = [storyboard instantiateInitialViewController];
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self presentViewController:viewController animated:YES completion:nil];
+    });
+}
 @end
